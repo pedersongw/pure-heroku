748dbc3a88cc78b901c8f933fe9ad7a499f14b31
Garrett Pederson <pedersongw@gmail.com>
2021-11-05 16:04:47 -0400

    added mongoose declaration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const MONGO_STRING = process.env.MONGODB_URI;
