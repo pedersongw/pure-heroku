478faa50635d687aeba2da2c05db4e15e9b2c01a
Garrett Pederson <pedersongw@gmail.com>
2021-11-05 15:42:49 -0400

    attempting heroku deploy

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config()
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`)
+  );
+});
+// }
