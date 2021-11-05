877c56320695eed8f2d6d319a6eeda80b2bd8340
Garrett Pederson <pedersongw@gmail.com>
2021-11-05 16:06:55 -0400

    mongoose

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => {
-    console.log("Successfully connected to the database");
+    window.alert("Successfully connected to the database");
   })
   .catch((err) => {
     console.log("Could not connect to the database. Error...", err);
