0e03c6a4872efa9f3aeac903ca0fd40f49d24de5
Garrett Pederson <pedersongw@gmail.com>
2021-11-06 13:42:07 -0400

    added server console.log when button clicked

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 const RenameSchema = require("../models/rename.js");
 
 exports.message = (req, res) => {
+  console.log("Get request received");
   res.send(" GET request recieved. This is a response from the backend.");
 };
