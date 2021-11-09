0d8193b09625352ac49118eb5b6b80a0e221951f
Garrett Pederson <pedersongw@gmail.com>
2021-11-09 12:26:27 -0500

    iliterally don't know

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 require("./routes/router.js")(app);
 
